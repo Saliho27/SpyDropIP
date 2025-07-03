cfe5badf73b18d529508155574228b7f0d44f664
Saliho27 <121923166+Saliho27@users.noreply.github.com>
2025-07-03 22:22:51 +0300

    Create SpyDropIP.py

---
+++ SpyDropIP.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "مرحبًا بك في أداة بايثون الخاصة بي على GitHub!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
